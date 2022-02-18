d060f55b89a06cc5805bdf133af0cc1ec9e2ea21
JohnWickKeanue <92579700+JohnWickKeanue@users.noreply.github.com>
2022-02-18 19:47:08 +0530

    Update gplinks_bypass.py

--- bot/plugins/gplinks_bypass.py
+++ bot/plugins/gplinks_bypass.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 
-url = "https://gplinks.co/0QwPXXE" # eg: https://gplinks.co/XXXX
+ # eg: https://gplinks.co/XXXX
 
 # ==============================================
 
